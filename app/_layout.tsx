3211a218712f5a863c16035a3a7fc9134f836897
rafa.bravv <rafa.bravv@gmail.com>
2026-03-11 20:51:16 +0000

    🛠️Error con react-native-reanimated solucionado - react-native-reanimated debía ser importado al principio del archivo _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import 'react-native-reanimated';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 import { StripeProvider } from '@stripe/stripe-react-native';
 import Mapbox from '@rnmapbox/maps';
 
